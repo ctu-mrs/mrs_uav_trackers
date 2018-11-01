8334ff8e187871a41e6ba72791821cea822f51f4
Daniel Heřt <hertdani@fel.cvut.cz>
2018-11-01 16:08:33 +0100

    Snap MPC, collision avoidance is buggy, but the drone flies

--- src/mpc_tracker/cvx_wrapper.cpp
+++ src/mpc_tracker/cvx_wrapper.cpp
@@ -24,8 +24,8 @@ CvxWrapper::CvxWrapper(bool verbose, int max_iters, std::vector<double> tempQ, s
   set_defaults();
   setup_indexing();
   setup_indexed_params();
-  dim = dimension * 3;
-  if (dim > 6 || dim < 0) {
+  dim = dimension * 4;
+  if (dim > 8 || dim < 0) {
     ROS_ERROR("CvxWrapper - parameter dim should be 0, 1 or 2 !!! setting to 0");
     dim = 0;
   }
@@ -42,8 +42,8 @@ CvxWrapper::CvxWrapper(bool verbose, int max_iters, std::vector<double> tempQ, s
   }
   settings.max_iters = max_iters;
 
-  if (tempQ.size() == 3) {
-    for (int i = 0; i < 3; i++) {
+  if (tempQ.size() == 4) {
+    for (int i = 0; i < 4; i++) {
       if (tempQ[i] >= 0 && std::isfinite(tempQ[i])) {
         params.Q[i] = tempQ[i];
       } else {
@@ -56,6 +56,7 @@ CvxWrapper::CvxWrapper(bool verbose, int max_iters, std::vector<double> tempQ, s
     params.Q[0] = 5000;
     params.Q[1] = 0;
     params.Q[2] = 0;
+    params.Q[3] = 0;
   }
 
   if (tempR.size() == 1) {
@@ -88,30 +89,40 @@ CvxWrapper::CvxWrapper(bool verbose, int max_iters, std::vector<double> tempQ, s
   params.A[0] = 1;
   params.A[1] = 1;
   params.A[2] = 1;
-  params.A[3] = dt2;
+  params.A[3] = 1;
   params.A[4] = dt2;
-  params.A[5] = 0.5 * dt2 * dt2;
+  params.A[5] = dt2;
+  params.A[6] = dt2;
+  params.A[7] = 0.5 * dt2 * dt2;
+  params.A[8] = 0.5 * dt2 * dt2;
+  params.A[8] = 1/6 * dt2 * dt2 * dt2;
 
   params.B[0] = dt2;
 
   params.Af[0] = 1;
   params.Af[1] = 1;
   params.Af[2] = 1;
-  params.Af[3] = dt;
+  params.Af[3] = 1;
   params.Af[4] = dt;
-  params.Af[5] = 0.5 * dt * dt;
+  params.Af[5] = dt;
+  params.Af[6] = dt;
+  params.Af[7] = 0.5 * dt * dt;
+  params.Af[8] = 0.5 * dt * dt;
+  params.Af[9] = 1/6 * dt * dt * dt;
 
   params.Bf[0] = dt;
 
   ROS_INFO("Cvx wrapper initiated");
 }
-void CvxWrapper::setLimits(double max_speed, double min_speed, double max_acc, double min_acc, double max_jerk, double min_jerk, int q_vel) {
+void CvxWrapper::setLimits(double max_speed, double min_speed, double max_acc, double min_acc, double max_jerk, double min_jerk, double max_snap, double min_snap, int q_vel) {
   params.x_max_2[0] = max_speed;
   params.x_min_2[0] = min_speed;
   params.x_max_3[0] = max_acc;
   params.x_min_3[0] = min_acc;
-  params.u_max[0]   = max_jerk;
-  params.u_min[0]   = min_jerk;
+  params.x_max_4[0] = max_jerk;
+  params.x_min_4[0] = min_jerk;
+  params.u_max[0]   = max_snap;
+  params.u_min[0]   = min_snap;
   params.Q[1]       = q_vel;
 }
 
@@ -119,6 +130,7 @@ void CvxWrapper::setInitialState(MatrixXd& x) {
   params.x_0[0] = x(0, 0);
   params.x_0[1] = x(1, 0);
   params.x_0[2] = x(2, 0);
+  params.x_0[3] = x(3, 0);
 }
 void CvxWrapper::loadReference(MatrixXd& reference) {
   for (int i = 0; i < horizon_len; i++) {
@@ -130,9 +142,10 @@ int CvxWrapper::solveCvx() {
 }
 void CvxWrapper::getStates(MatrixXd& future_traj) {
   for (int i = 0; i < horizon_len; i++) {
-    future_traj(0 + dim + (i * 9)) = *(vars.x[i + 1]);
-    future_traj(1 + dim + (i * 9)) = *(vars.x[i + 1] + 1);
-    future_traj(2 + dim + (i * 9)) = *(vars.x[i + 1] + 2);
+    future_traj(0 + dim + (i * 12)) = *(vars.x[i + 1]);
+    future_traj(1 + dim + (i * 12)) = *(vars.x[i + 1] + 1);
+    future_traj(2 + dim + (i * 12)) = *(vars.x[i + 1] + 2);
+    future_traj(3 + dim + (i * 12)) = *(vars.x[i + 1] + 3);
   }
 }
 double CvxWrapper::getFirstControlInput() {
