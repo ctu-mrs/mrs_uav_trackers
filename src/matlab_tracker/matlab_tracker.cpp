2be33216391d3bf1b79e248dc7379ec41e9f81cc
Tomas Baca <klaxalk@gmail.com>
2019-03-12 14:15:37 +0100

    added matlab tracker

---
+++ src/matlab_tracker/matlab_tracker.cpp
@@ -0,0 +1,440 @@
+#include <ros/ros.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+
+#include <mrs_msgs/TrackerDiagnostics.h>
+#include <mrs_msgs/TrackerPointStamped.h>
+#include <mrs_uav_manager/Tracker.h>
+
+#include <tf/transform_datatypes.h>
+#include <mutex>
+
+#include <commons.h>
+
+#include <mrs_lib/ParamLoader.h>
+#include <mrs_lib/Profiler.h>
+
+#define STOP_THR 1e-3
+
+namespace mrs_trackers
+{
+
+  /* //{ class MatlabTracker */
+
+  class MatlabTracker : public mrs_uav_manager::Tracker {
+  public:
+    MatlabTracker(void);
+
+    virtual void initialize(const ros::NodeHandle &parent_nh, mrs_uav_manager::SafetyArea_t const *safety_area);
+    virtual bool activate(const mrs_msgs::PositionCommand::ConstPtr &cmd);
+    virtual void deactivate(void);
+
+    virtual const mrs_msgs::PositionCommand::ConstPtr update(const nav_msgs::Odometry::ConstPtr &msg);
+    virtual const mrs_msgs::TrackerStatus::Ptr        getStatus();
+    virtual const std_srvs::SetBoolResponse::ConstPtr enableCallbacks(const std_srvs::SetBoolRequest::ConstPtr &cmd);
+    virtual void                                      switchOdometrySource(const nav_msgs::Odometry::ConstPtr &msg);
+
+    virtual const mrs_msgs::Vec4Response::ConstPtr goTo(const mrs_msgs::Vec4Request::ConstPtr &cmd);
+    virtual const mrs_msgs::Vec4Response::ConstPtr goToRelative(const mrs_msgs::Vec4Request::ConstPtr &cmd);
+    virtual const mrs_msgs::Vec1Response::ConstPtr goToAltitude(const mrs_msgs::Vec1Request::ConstPtr &cmd);
+    virtual const mrs_msgs::Vec1Response::ConstPtr setYaw(const mrs_msgs::Vec1Request::ConstPtr &cmd);
+    virtual const mrs_msgs::Vec1Response::ConstPtr setYawRelative(const mrs_msgs::Vec1Request::ConstPtr &cmd);
+
+    virtual bool goTo(const mrs_msgs::TrackerPointStampedConstPtr &msg);
+    virtual bool goToRelative(const mrs_msgs::TrackerPointStampedConstPtr &msg);
+    virtual bool goToAltitude(const std_msgs::Float64ConstPtr &msg);
+    virtual bool setYaw(const std_msgs::Float64ConstPtr &msg);
+    virtual bool setYawRelative(const std_msgs::Float64ConstPtr &msg);
+
+    virtual const mrs_msgs::TrackerConstraintsResponse::ConstPtr setConstraints(const mrs_msgs::TrackerConstraintsRequest::ConstPtr &cmd);
+
+    virtual const std_srvs::TriggerResponse::ConstPtr hover(const std_srvs::TriggerRequest::ConstPtr &cmd);
+
+  private:
+    bool callbacks_enabled = true;
+
+  private:
+    nav_msgs::Odometry odometry;
+    bool               got_odometry = false;
+    std::mutex         mutex_odometry;
+
+    double odometry_x;
+    double odometry_y;
+    double odometry_z;
+    double odometry_yaw;
+    double odometry_roll;
+    double odometry_pitch;
+
+  private:
+    // tracker's inner states
+    int    tracker_loop_rate_;
+    double tracker_dt_;
+    bool   is_initialized;
+    bool   is_active;
+    bool   first_iter;
+
+  private:
+    // dynamical constraints
+    double     yaw_rate_;
+    std::mutex mutex_constraints;
+
+  private:
+    // desired goal
+    double             have_goal = false;
+    std::mutex         mutex_goal;
+    nav_msgs::Odometry matlab_goal;
+
+    mrs_msgs::PositionCommand position_output;
+
+  private:
+    ros::Subscriber subscriber_matlab;
+    void            callbackMatlab(const nav_msgs::Odometry &msg);
+
+  private:
+    mrs_lib::Profiler *profiler;
+    bool               profiler_enabled_ = false;
+  };
+
+  MatlabTracker::MatlabTracker(void) : is_initialized(false), is_active(false) {
+  }
+
+  //}
+
+  // | -------------- tracker's interface routines -------------- |
+
+  /* //{ initialize() */
+
+  void MatlabTracker::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] mrs_uav_manager::SafetyArea_t const *safety_area) {
+
+    ros::NodeHandle nh_(parent_nh, "matlab_tracker");
+
+    ros::Time::waitForValid();
+
+    // --------------------------------------------------------------
+    // |                       load parameters                      |
+    // --------------------------------------------------------------
+
+    mrs_lib::ParamLoader param_loader(nh_, "MatlabTracker");
+
+    param_loader.load_param("enable_profiler", profiler_enabled_);
+
+    // --------------------------------------------------------------
+    // |                          profiler                          |
+    // --------------------------------------------------------------
+
+    profiler = new mrs_lib::Profiler(nh_, "matlabtracker", profiler_enabled_);
+
+    // --------------------------------------------------------------
+    // |                         subscribers                        |
+    // --------------------------------------------------------------
+
+    subscriber_matlab = nh_.subscribe("goal_in", 1, &MatlabTracker::callbackMatlab, this, ros::TransportHints().tcpNoDelay());
+
+    if (!param_loader.loaded_successfully()) {
+      ROS_ERROR("[MatlabTracker]: Could not load all parameters!");
+      ros::shutdown();
+    }
+
+    is_initialized = true;
+
+    ROS_INFO("[MatlabTracker]: initialized");
+  }
+
+  //}
+
+  /* //{ activate() */
+
+  bool MatlabTracker::activate([[maybe_unused]] const mrs_msgs::PositionCommand::ConstPtr &cmd) {
+
+    if (!got_odometry) {
+      ROS_ERROR("[MatlabTracker]: can't activate(), odometry not set");
+      return false;
+    }
+
+    std::scoped_lock lock(mutex_goal);
+
+    if (!have_goal) {
+
+      ROS_ERROR("[MatlabTracker]: cannot activate, missing Matlab command");
+      return false;
+    }
+
+    // --------------------------------------------------------------
+    // |              yaw initial condition  prediction             |
+    // --------------------------------------------------------------
+
+    is_active = true;
+
+    ROS_INFO("[MatlabTracker]: activated");
+
+    return true;
+  }
+
+  //}
+
+  /* //{ deactivate() */
+
+  void MatlabTracker::deactivate(void) {
+
+    is_active = false;
+    have_goal = false;
+
+    ROS_INFO("[MatlabTracker]: deactivated");
+  }
+
+  //}
+
+  /* //{ update() */
+
+  const mrs_msgs::PositionCommand::ConstPtr MatlabTracker::update(const nav_msgs::Odometry::ConstPtr &msg) {
+
+    mrs_lib::Routine profiler_routine = profiler->createRoutine("update");
+
+    {
+      std::scoped_lock lock(mutex_odometry);
+
+      odometry   = *msg;
+      odometry_x = odometry.pose.pose.position.x;
+      odometry_y = odometry.pose.pose.position.y;
+      odometry_z = odometry.pose.pose.position.z;
+
+      // calculate the euler angles
+      tf::Quaternion quaternion_odometry;
+      quaternionMsgToTF(odometry.pose.pose.orientation, quaternion_odometry);
+      tf::Matrix3x3 m(quaternion_odometry);
+      m.getRPY(odometry_roll, odometry_pitch, odometry_yaw);
+
+      got_odometry = true;
+    }
+
+    // up to this part the update() method is evaluated even when the tracker is not active
+    if (!is_active) {
+      return mrs_msgs::PositionCommand::Ptr();
+    }
+
+    position_output.header.stamp    = ros::Time::now();
+    position_output.header.frame_id = "local_origin";
+
+    {
+      std::scoped_lock lock(mutex_odometry, mutex_goal);
+
+      position_output.position.x = matlab_goal.pose.pose.position.x;
+      position_output.position.y = matlab_goal.pose.pose.position.y;
+      position_output.position.z = matlab_goal.pose.pose.position.z;
+
+      position_output.velocity.x = matlab_goal.twist.twist.linear.x;
+      position_output.velocity.y = matlab_goal.twist.twist.linear.y;
+      position_output.velocity.z = matlab_goal.twist.twist.linear.z;
+
+      position_output.acceleration.x = matlab_goal.twist.twist.angular.x;
+      position_output.acceleration.y = matlab_goal.twist.twist.angular.y;
+      position_output.acceleration.z = matlab_goal.twist.twist.angular.z;
+
+      position_output.yaw     = matlab_goal.pose.pose.orientation.x;
+      position_output.yaw_dot = matlab_goal.pose.pose.orientation.y;
+
+      position_output.use_euler_attitude = 1;
+      position_output.use_position       = 1;
+      position_output.use_velocity       = 1;
+      position_output.use_acceleration   = 1;
+    }
+
+    return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_output));
+  }
+
+  //}
+
+  /* //{ getStatus() */
+
+  const mrs_msgs::TrackerStatus::Ptr MatlabTracker::getStatus() {
+
+    if (is_initialized) {
+
+      mrs_msgs::TrackerStatus::Ptr tracker_status(new mrs_msgs::TrackerStatus);
+
+      if (is_active) {
+        tracker_status->active = mrs_msgs::TrackerStatus::ACTIVE;
+      } else {
+        tracker_status->active = mrs_msgs::TrackerStatus::NONACTIVE;
+      }
+
+      return tracker_status;
+    } else {
+
+      return mrs_msgs::TrackerStatus::Ptr();
+    }
+  }
+
+  //}
+
+  /* //{ enableCallbacks() */
+
+  const std_srvs::SetBoolResponse::ConstPtr MatlabTracker::enableCallbacks(const std_srvs::SetBoolRequest::ConstPtr &cmd) {
+
+    char                      message[100];
+    std_srvs::SetBoolResponse res;
+
+    if (cmd->data != callbacks_enabled) {
+
+      callbacks_enabled = cmd->data;
+
+      sprintf((char *)&message, "Callbacks %s", callbacks_enabled ? "enabled" : "disabled");
+
+      ROS_INFO("[MatlabTracker]: %s", message);
+
+    } else {
+
+      sprintf((char *)&message, "Callbacks were already %s", callbacks_enabled ? "enabled" : "disabled");
+    }
+
+    res.message = message;
+    res.success = true;
+
+    return std_srvs::SetBoolResponse::ConstPtr(new std_srvs::SetBoolResponse(res));
+  }
+
+  //}
+
+  /* switchOdometrySource() //{ */
+
+  void MatlabTracker::switchOdometrySource([[maybe_unused]] const nav_msgs::Odometry::ConstPtr &msg) {
+  }
+
+  //}
+
+  // | -------------- setpoint topics and services -------------- |
+
+  /* //{ goTo() service */
+
+  const mrs_msgs::Vec4Response::ConstPtr MatlabTracker::goTo([[maybe_unused]] const mrs_msgs::Vec4Request::ConstPtr &cmd) {
+
+    return mrs_msgs::Vec4Response::Ptr();
+  }
+
+  //}
+
+  /* //{ goTo() topic */
+
+  bool MatlabTracker::goTo([[maybe_unused]] const mrs_msgs::TrackerPointStampedConstPtr &msg) {
+
+    return false;
+  }
+
+  //}
+
+  /* //{ goToRelative() service */
+
+  const mrs_msgs::Vec4Response::ConstPtr MatlabTracker::goToRelative([[maybe_unused]] const mrs_msgs::Vec4Request::ConstPtr &cmd) {
+
+    return mrs_msgs::Vec4Response::Ptr();
+  }
+
+  //}
+
+  /* //{ goToRelative() topic */
+
+  bool MatlabTracker::goToRelative([[maybe_unused]] const mrs_msgs::TrackerPointStampedConstPtr &msg) {
+
+    return false;
+  }
+
+  //}
+
+  /* //{ goToAltitude() service */
+
+  const mrs_msgs::Vec1Response::ConstPtr MatlabTracker::goToAltitude([[maybe_unused]] const mrs_msgs::Vec1Request::ConstPtr &cmd) {
+
+    return mrs_msgs::Vec1Response::Ptr();
+  }
+
+  //}
+
+  /* //{ goToAltitude() topic */
+
+  bool MatlabTracker::goToAltitude([[maybe_unused]] const std_msgs::Float64ConstPtr &msg) {
+
+    return false;
+  }
+
+  //}
+
+  /* //{ setYaw() service */
+
+  const mrs_msgs::Vec1Response::ConstPtr MatlabTracker::setYaw([[maybe_unused]] const mrs_msgs::Vec1Request::ConstPtr &cmd) {
+
+    return mrs_msgs::Vec1Response::Ptr();
+  }
+
+  //}
+
+  /* //{ setYaw() topic */
+
+  bool MatlabTracker::setYaw([[maybe_unused]] const std_msgs::Float64ConstPtr &msg) {
+
+    return false;
+  }
+
+  //}
+
+  /* //{ setYawRelative() service */
+
+  const mrs_msgs::Vec1Response::ConstPtr MatlabTracker::setYawRelative([[maybe_unused]] const mrs_msgs::Vec1Request::ConstPtr &cmd) {
+
+    return mrs_msgs::Vec1Response::Ptr();
+  }
+
+  //}
+
+  /* //{ setYawRelative() topic */
+
+  bool MatlabTracker::setYawRelative([[maybe_unused]] const std_msgs::Float64ConstPtr &msg) {
+
+    return false;
+  }
+
+  //}
+
+  /* //{ hover() service */
+
+  const std_srvs::TriggerResponse::ConstPtr MatlabTracker::hover([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+
+    return std_srvs::TriggerResponse::Ptr();
+  }
+
+  //}
+
+  /* //{ setConstraints() service */
+
+  const mrs_msgs::TrackerConstraintsResponse::ConstPtr MatlabTracker::setConstraints([
+      [maybe_unused]] const mrs_msgs::TrackerConstraintsRequest::ConstPtr &cmd) {
+
+    return mrs_msgs::TrackerConstraintsResponse::Ptr();
+  }
+
+  //}
+
+  // | --------------------- custom methods --------------------- |
+
+  /* callbackJoystick() //{ */
+
+  void MatlabTracker::callbackMatlab(const nav_msgs::Odometry &msg) {
+
+    if (!is_initialized)
+      return;
+
+    std::scoped_lock lock(mutex_goal);
+
+    mrs_lib::Routine profiler_routine = profiler->createRoutine("callbackMatlab");
+
+    matlab_goal = msg;
+
+    have_goal = true;
+  }
+
+  //}
+
+}  // namespace mrs_trackers
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_trackers::MatlabTracker, mrs_uav_manager::Tracker)
