4dc7e41d9b555332c0097c0288573673ec6203e1
Vit Kratky <kratkvit@fel.cvut.cz>
2019-07-09 18:21:41 +0200

    Add implementation of bumper for vertical direction

--- src/joy_tracker/joy_tracker.cpp
+++ src/joy_tracker/joy_tracker.cpp
@@ -121,21 +121,27 @@ private:
   bool            got_bumper = false;
 
   mrs_msgs::ObstacleSectors bumper_data;
+  mrs_msgs::ObstacleSectors bumper_previous_data;
   std::mutex                mutex_bumper;
 
-  bool bumper_enabled_           = false;
+  bool bumper_enabled_ = false;
   /* bool bumper_hugging_enabled_   = false; */
   /* bool bumper_repulsion_enabled_ = false; */
   /* bool repulsing                 = false; */
   /* uint repulsing_from; */
 
-  /* double bumper_horizontal_distance_; */
-  /* double bumper_vertical_distance_; */
+  double bumper_horizontal_distance_;
+  double bumper_vertical_distance_;
 
-  /* double bumper_repulsion_horizontal_distance_; */
-  /* double bumper_repulsion_horizontal_offset_; */
-  /* double bumper_repulsion_vertical_distance_; */
-  /* double bumper_repulsion_vertical_offset_; */
+  double bumper_repulsion_horizontal_distance_;
+  double bumper_critical_horizontal_distance_;
+  double bumper_repulsion_horizontal_offset_;
+  double bumper_critical_vertical_distance_;
+  double bumper_repulsion_vertical_distance_;
+  double bumper_repulsion_vertical_offset_;
+
+  bool bumperPushFromObstacle();
+  int  bumperGetSectorId(const double x, const double y, const double z);
 
 private:
   mrs_lib::Profiler *profiler;
@@ -617,6 +623,140 @@ void JoyTracker::callbackBumper(const mrs_msgs::ObstacleSectorsConstPtr &msg) {
 }
 
 //}
+
+/* bumperGetSectorId() //{ */
+
+int JoyTracker::bumperGetSectorId(const double x, const double y, [[maybe_unused]] const double z) {
+
+  // heading of the point in drone frame
+  double point_heading_horizontal = atan2(y, x);
+
+  point_heading_horizontal += 2 * M_PI;
+
+  // if point_heading_horizontal is greater then 2*M_PI mod it
+  if (fabs(point_heading_horizontal) >= 2 * M_PI) {
+    point_heading_horizontal = fmod(point_heading_horizontal, 2 * M_PI);
+  }
+
+  // heading of the right edge of the first sector
+  double sector_size = 2 * M_PI / double(bumper_data.n_horizontal_sectors);
+
+  // calculate the idx
+  int idx = floor((point_heading_horizontal + (sector_size / 2.0)) / sector_size);
+
+  if (uint(idx) > bumper_data.n_horizontal_sectors - 1) {
+    idx -= bumper_data.n_horizontal_sectors;
+  }
+
+  return idx;
+}
+
+//}
+//
+/* bumperPushFromObstacle() //{ */
+
+bool JoyTracker::bumperPushFromObstacle(void) {
+
+  if (!bumper_enabled_) {
+    return true;
+  }
+
+  if (!got_bumper) {
+    return true;
+  }
+
+  std::scoped_lock lock(mutex_bumper);
+
+  double sector_size = 2 * M_PI / double(bumper_data.n_horizontal_sectors);
+
+  double min_distance                  = 10e9;
+  double repulsion_distance            = 10e9;
+  double obst_dist_below               = 10e9;
+  double obst_dist_above               = 10e9;
+  double horizontal_collision_detected = false;
+  double ver_speed_correction_below    = 0;
+  double ver_speed_correction_above    = 0;
+
+
+  double vertical_collision_detected = false;
+
+  for (uint i = 0; i < bumper_data.n_horizontal_sectors; i++) {
+
+    if (bumper_data.sectors[i] < 0) {
+      continue;
+    }
+
+    // if the sector is under critical distance
+    if (bumper_data.sectors[i] <= bumper_repulsion_horizontal_distance_) {
+
+      // get the desired direction of motion
+      double oposite_direction  = double(i) * sector_size + M_PI;
+      int    oposite_sector_idx = bumperGetSectorId(cos(oposite_direction), sin(oposite_direction), 0);
+
+      horizontal_collision_detected = true;
+    }
+  }
+
+  bool   collision_above             = false;
+  bool   collision_below             = false;
+
+  // check for vertical collision down
+  if (bumper_data.sectors[bumper_data.n_horizontal_sectors] > 0 &&
+      bumper_data.sectors[bumper_data.n_horizontal_sectors] <= bumper_repulsion_vertical_distance_) {
+
+    ROS_INFO_THROTTLE(1.0, "[JoyTracker]: bumper: potential collision below");
+    collision_below             = true;
+    vertical_collision_detected = true;
+    obst_dist_below             = bumper_data.sectors[bumper_data.n_horizontal_sectors];
+    if (bumper_data.sectors[bumper_data.n_horizontal_sectors] <= bumper_critical_vertical_distance_) {  // below critical distance
+      if (bumper_data.sectors[bumper_data.n_horizontal_sectors] >
+          bumper_previous_data.sectors[bumper_data.n_horizontal_sectors]) {  // further than in previous step
+        ver_speed_correction_below = 0;
+      } else {                             // closer than previous step
+        ver_speed_correction_below = 0.1;  // TODO add adpation based on the distance
+      }
+    } else {  // inside detection radius
+      ver_speed_correction_below = -1.0 * (bumper_data.sectors[bumper_data.n_horizontal_sectors] - bumper_critical_vertical_distance_) /
+                                   (bumper_repulsion_vertical_distance_ - bumper_critical_vertical_distance_);  // normalized
+    }
+  }
+
+  // check for vertical collision up
+  if (bumper_data.sectors[bumper_data.n_horizontal_sectors + 1] > 0 &&
+      bumper_data.sectors[bumper_data.n_horizontal_sectors + 1] <= bumper_repulsion_vertical_distance_) {
+
+    ROS_INFO_THROTTLE(1.0, "[JoyTracker]: bumper: potential collision above");
+    collision_above             = true;
+    vertical_collision_detected = true;
+    obst_dist_above             = bumper_data.sectors[bumper_data.n_horizontal_sectors + 1];
+    if (bumper_data.sectors[bumper_data.n_horizontal_sectors + 1] <= bumper_critical_vertical_distance_) {  // below critical distance
+      if (bumper_data.sectors[bumper_data.n_horizontal_sectors + 1] >
+          bumper_previous_data.sectors[bumper_data.n_horizontal_sectors + 1]) {  // further than in previous step
+        ver_speed_correction_above = 0;
+      } else {                              // closer than previous step
+        ver_speed_correction_above = -0.1;  // TODO add adpation based on the distance
+      }
+    } else {  // inside detection radius
+      ver_speed_correction_above = 1.0 * (bumper_data.sectors[bumper_data.n_horizontal_sectors] - bumper_critical_vertical_distance_) /
+                                   (bumper_repulsion_vertical_distance_ - bumper_critical_vertical_distance_);  // normalized
+    }
+  }
+
+  if (obst_dist_above < bumper_critical_vertical_distance_ && obst_dist_below < bumper_critical_vertical_distance_) { // in critical distance below and above concurrently
+    vertical_speed_ = 0.05 * (obst_dist_above - obst_dist_below) / bumper_critical_vertical_distance_;  // replace by coefficient
+  } else {
+    ROS_INFO("[JoyTracker]: vertical_speed = %.2f, limits = [%.2f, %.2f]", vertical_speed_, ver_speed_correction_below, ver_speed_correction_above);
+    vertical_speed_ = fmax(fmin(vertical_speed_, ver_speed_correction_above), ver_speed_correction_below);
+    ROS_INFO("[JoyTracker]: Resulting vertical_speed = %.2f", vertical_speed_);
+  }
+  
+  bumper_previous_data = bumper_data;  // TODO: check correctness of assignment
+  return false;
+}
+
+//}
+
+
 }  // namespace joy_tracker
 
 }  // namespace mrs_trackers
