a750df661caa220eb8913a0d033867c6b8f8e598
Tomas Baca <klaxalk@gmail.com>
2021-08-03 16:13:10 +0200

    added midair activation

---
+++ src/midair_activation_tracker/midair_activation_tracker.cpp
@@ -0,0 +1,308 @@
+#define VERSION "1.0.1.0"
+
+/* includes //{ */
+
+#include <ros/ros.h>
+
+#include <mrs_uav_managers/tracker.h>
+
+#include <mrs_lib/profiler.h>
+#include <mrs_lib/mutex.h>
+#include <mrs_lib/attitude_converter.h>
+#include <mrs_lib/geometry/cyclic.h>
+#include <mrs_lib/geometry/misc.h>
+#include <mrs_lib/param_loader.h>
+
+//}
+
+namespace mrs_uav_trackers
+{
+
+namespace midair_activation_tracker
+{
+
+/* //{ class MidairActivationTracker */
+
+class MidairActivationTracker : public mrs_uav_managers::Tracker {
+public:
+  ~MidairActivationTracker(){};
+
+  void initialize(const ros::NodeHandle &parent_nh, const std::string uav_name, std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers);
+  std::tuple<bool, std::string> activate(const mrs_msgs::PositionCommand::ConstPtr &last_position_cmd);
+  void                          deactivate(void);
+  bool                          resetStatic(void);
+
+  const mrs_msgs::PositionCommand::ConstPtr update(const mrs_msgs::UavState::ConstPtr &uav_state, const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd);
+  const mrs_msgs::TrackerStatus             getStatus();
+  const std_srvs::SetBoolResponse::ConstPtr enableCallbacks(const std_srvs::SetBoolRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr switchOdometrySource(const mrs_msgs::UavState::ConstPtr &new_uav_state);
+
+  const mrs_msgs::ReferenceSrvResponse::ConstPtr           setReference(const mrs_msgs::ReferenceSrvRequest::ConstPtr &cmd);
+  const mrs_msgs::VelocityReferenceSrvResponse::ConstPtr   setVelocityReference(const mrs_msgs::VelocityReferenceSrvRequest::ConstPtr &cmd);
+  const mrs_msgs::TrajectoryReferenceSrvResponse::ConstPtr setTrajectoryReference(const mrs_msgs::TrajectoryReferenceSrvRequest::ConstPtr &cmd);
+
+  const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr setConstraints(const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &cmd);
+
+  const std_srvs::TriggerResponse::ConstPtr hover(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr startTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr stopTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr resumeTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr gotoTrajectoryStart(const std_srvs::TriggerRequest::ConstPtr &cmd);
+
+private:
+  bool callbacks_enabled_ = true;
+
+  std::string _version_;
+  std::string _uav_name_;
+
+  std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers_;
+
+  // | ---------------- the tracker's inner state --------------- |
+
+  bool is_initialized_ = false;
+  bool is_active_      = false;
+};
+
+//}
+
+// | -------------- tracker's interface routines -------------- |
+
+/* //{ initialize() */
+
+void MidairActivationTracker::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const std::string uav_name,
+                                         [[maybe_unused]] std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers) {
+
+  _uav_name_             = uav_name;
+  this->common_handlers_ = common_handlers;
+
+  ros::NodeHandle nh_(parent_nh, "midair_activation_tracker");
+
+  ros::Time::waitForValid();
+
+  mrs_lib::ParamLoader param_loader(nh_, "MidairActivationTracker");
+
+  param_loader.loadParam("version", _version_);
+
+  if (_version_ != VERSION) {
+
+    ROS_ERROR("[MidairActivationTracker]: the version of the binary (%s) does not match the config file (%s), please build me!", VERSION, _version_.c_str());
+    ros::shutdown();
+  }
+
+  // | --------------------- finish the init -------------------- |
+
+  is_initialized_ = true;
+
+  ROS_INFO("[MidairActivationTracker]: initialized, version %s", VERSION);
+}
+
+//}
+
+/* //{ activate() */
+
+std::tuple<bool, std::string> MidairActivationTracker::activate([[maybe_unused]] const mrs_msgs::PositionCommand::ConstPtr &last_position_cmd) {
+
+  std::stringstream ss;
+
+  is_active_ = true;
+
+  ss << "activated";
+  ROS_INFO_STREAM("[MidairActivationTracker]: " << ss.str());
+
+  return std::tuple(true, ss.str());
+}
+
+//}
+
+/* //{ deactivate() */
+
+void MidairActivationTracker::deactivate(void) {
+
+  is_active_ = false;
+
+  ROS_INFO("[MidairActivationTracker]: deactivated");
+}
+
+//}
+
+/* //{ resetStatic() */
+
+bool MidairActivationTracker::resetStatic(void) {
+
+  return false;
+}
+
+//}
+
+/* //{ update() */
+
+const mrs_msgs::PositionCommand::ConstPtr MidairActivationTracker::update(const mrs_msgs::UavState::ConstPtr &                        uav_state,
+                                                                          [[maybe_unused]] const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd) {
+
+  // up to this part the update() method is evaluated even when the tracker is not active
+  if (!is_active_) {
+    return mrs_msgs::PositionCommand::Ptr();
+  }
+
+  mrs_msgs::PositionCommand position_cmd;
+
+  position_cmd.header.frame_id = uav_state->header.frame_id;
+  position_cmd.header.stamp    = ros::Time::now();
+
+  position_cmd.position.x = uav_state->pose.position.x;
+  position_cmd.position.y = uav_state->pose.position.y;
+  position_cmd.position.z = uav_state->pose.position.z;
+
+  position_cmd.velocity.x = uav_state->velocity.linear.x;
+  position_cmd.velocity.y = uav_state->velocity.linear.y;
+  position_cmd.velocity.z = uav_state->velocity.linear.z;
+
+  try {
+    position_cmd.heading = mrs_lib::AttitudeConverter(uav_state->pose.orientation).getHeading();
+  }
+  catch (...) {
+    position_cmd.heading = mrs_lib::AttitudeConverter(uav_state->pose.orientation).getYaw();
+    ROS_WARN_THROTTLE(1.0, "[MidairActivationTracker]: could not get heading");
+  }
+
+  position_cmd.use_position_vertical   = true;
+  position_cmd.use_position_horizontal = true;
+
+  position_cmd.use_velocity_vertical   = true;
+  position_cmd.use_velocity_horizontal = true;
+
+  position_cmd.use_heading = true;
+
+  ROS_WARN_THROTTLE(0.1, "[MidairActivationTracker]: outputting cmd");
+
+  return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_cmd));
+}
+
+//}
+
+/* //{ getStatus() */
+
+const mrs_msgs::TrackerStatus MidairActivationTracker::getStatus() {
+
+  mrs_msgs::TrackerStatus tracker_status;
+
+  tracker_status.active            = is_active_;
+  tracker_status.callbacks_enabled = callbacks_enabled_;
+
+  return tracker_status;
+}
+
+//}
+
+/* //{ enableCallbacks() */
+
+const std_srvs::SetBoolResponse::ConstPtr MidairActivationTracker::enableCallbacks([[maybe_unused]] const std_srvs::SetBoolRequest::ConstPtr &cmd) {
+
+  std_srvs::SetBoolResponse res;
+
+  res.message = "callbacks are always disabled";
+  res.success = true;
+
+  return std_srvs::SetBoolResponse::ConstPtr(new std_srvs::SetBoolResponse(res));
+}
+
+//}
+
+/* switchOdometrySource() //{ */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::switchOdometrySource([[maybe_unused]] const mrs_msgs::UavState::ConstPtr &new_uav_state) {
+
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ hover() */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::hover([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ startTrajectoryTracking() */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::startTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ stopTrajectoryTracking() */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::stopTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ resumeTrajectoryTracking() */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::resumeTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ gotoTrajectoryStart() */
+
+const std_srvs::TriggerResponse::ConstPtr MidairActivationTracker::gotoTrajectoryStart([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+
+//}
+
+/* //{ setConstraints() */
+
+const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr MidairActivationTracker::setConstraints([
+    [maybe_unused]] const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &cmd) {
+
+  mrs_msgs::DynamicsConstraintsSrvResponse res;
+
+  res.success = true;
+  res.message = "constraints updated";
+
+  return mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr(new mrs_msgs::DynamicsConstraintsSrvResponse(res));
+}
+
+//}
+
+/* //{ setReference() */
+
+const mrs_msgs::ReferenceSrvResponse::ConstPtr MidairActivationTracker::setReference([[maybe_unused]] const mrs_msgs::ReferenceSrvRequest::ConstPtr &cmd) {
+
+  return mrs_msgs::ReferenceSrvResponse::Ptr();
+}
+
+//}
+
+/* //{ setVelocityReference() */
+
+const mrs_msgs::VelocityReferenceSrvResponse::ConstPtr MidairActivationTracker::setVelocityReference([
+    [maybe_unused]] const mrs_msgs::VelocityReferenceSrvRequest::ConstPtr &cmd) {
+  return mrs_msgs::VelocityReferenceSrvResponse::Ptr();
+}
+
+//}
+
+/* //{ setTrajectoryReference() */
+
+const mrs_msgs::TrajectoryReferenceSrvResponse::ConstPtr MidairActivationTracker::setTrajectoryReference([
+    [maybe_unused]] const mrs_msgs::TrajectoryReferenceSrvRequest::ConstPtr &cmd) {
+  return mrs_msgs::TrajectoryReferenceSrvResponse::Ptr();
+}
+
+//}
+
+}  // namespace midair_activation_tracker
+
+}  // namespace mrs_uav_trackers
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_uav_trackers::midair_activation_tracker::MidairActivationTracker, mrs_uav_managers::Tracker)
