bbef5f7c0c06fd87787d132b5810c5c3dc7415a9
Tomas Baca <klaxalk@gmail.com>
2024-02-09 09:32:58 +0100

    updated the test format

--- test/compile_tests.sh
+++ test/compile_tests.sh
@@ -7,4 +7,4 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 # build the package
 catkin build --this # it has to be fully built normally before building with --catkin-make-args tests
-catkin build --this --no-deps --catkin-make-args tests
+catkin build --this -DMRS_ENABLE_TESTING=1 --no-deps --catkin-make-args tests
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,8 +5,4 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-# build the package
-catkin build --this # it has to be fully built normally before building with --catkin-make-args tests
-catkin build --this --no-deps --catkin-make-args tests
-
 catkin test --this -i -p 1 -s
