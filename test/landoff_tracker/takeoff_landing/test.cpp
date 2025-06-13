d635ac9e7b7cb0ba4f7cc90c2f348d667ac3deb5
Tomas Baca <Tomas Baca>
2025-06-13 16:55:30 +0200

    + tests

---
+++ test/landoff_tracker/takeoff_landing/test.cpp
@@ -0,0 +1,101 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+
+#include <mrs_uav_testing/test_generic.h>
+
+using namespace std::chrono_literals;
+
+class Tester : public mrs_uav_testing::TestGeneric {
+
+public:
+  Tester() : mrs_uav_testing::TestGeneric() {
+  }
+
+  bool test(void);
+};
+
+bool Tester::test(void) {
+
+  const std::string uav_name = "uav1";
+
+  std::shared_ptr<mrs_uav_testing::UAVHandler> uh;
+
+  {
+    auto [uhopt, message] = getUAVHandler(uav_name);
+
+    if (!uhopt) {
+      RCLCPP_ERROR(node_->get_logger(), "Failed obtain handler for '%s': '%s'", uav_name.c_str(), message.c_str());
+      return false;
+    }
+
+    uh = uhopt.value();
+  }
+
+  // | ------------------------- takeoff ------------------------ |
+
+  {
+    auto [success, message] = uh->takeoff();
+
+    if (!success) {
+      RCLCPP_ERROR(node_->get_logger(), "takeoff failed with message: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  this->sleep(1.0);
+
+  if (!uh->isFlyingNormally()) {
+    RCLCPP_ERROR(node_->get_logger(), "not flying normally");
+    return false;
+  }
+
+  // | --------------------- goto somewhere --------------------- |
+
+  {
+    auto [success, message] = uh->gotoRel(10, 1, 2, 1.5);
+
+    if (!success) {
+      RCLCPP_ERROR(node_->get_logger(), "goto failed with message: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  this->sleep(1.0);
+
+  if (!uh->isFlyingNormally()) {
+    RCLCPP_ERROR(node_->get_logger(), "not flying normally");
+    return false;
+  }
+
+  // | -------------------------- land -------------------------- |
+
+  {
+    auto [success, message] = uh->land();
+
+    if (!success) {
+      RCLCPP_ERROR(node_->get_logger(), "landing failed with message: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(int argc, char* argv[]) {
+
+  rclcpp::init(argc, argv);
+
+  bool test_result = true;
+
+  Tester tester;
+
+  test_result &= tester.test();
+
+  tester.sleep(2.0);
+
+  std::cout << "Test: reporting test results" << std::endl;
+
+  tester.reportTestResult(test_result);
+
+  tester.join();
+}
---
+++ test/mpc_tracker/mpc_tracker_goto/test.cpp
@@ -0,0 +1,84 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+
+#include <mrs_uav_testing/test_generic.h>
+
+using namespace std::chrono_literals;
+
+class Tester : public mrs_uav_testing::TestGeneric {
+
+public:
+  Tester() : mrs_uav_testing::TestGeneric() {
+  }
+
+  bool test(void);
+};
+
+bool Tester::test(void) {
+
+  const std::string uav_name = "uav1";
+
+  std::shared_ptr<mrs_uav_testing::UAVHandler> uh;
+
+  {
+    auto [uhopt, message] = getUAVHandler(uav_name);
+
+    if (!uhopt) {
+      RCLCPP_ERROR(node_->get_logger(), "Failed obtain handler for '%s': '%s'", uav_name.c_str(), message.c_str());
+      return false;
+    }
+
+    uh = uhopt.value();
+  }
+
+  {
+    auto [success, message] = uh->activateMidAir();
+
+    if (!success) {
+      RCLCPP_ERROR(node_->get_logger(), "midair activation failed with message: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  if (uh->getActiveTracker() != "MpcTracker") {
+    RCLCPP_ERROR(node_->get_logger(), "MpcTracker is not active");
+    return false;
+  }
+
+  {
+    auto [success, message] = uh->gotoRel(10, 1, 2, 1.5);
+
+    if (!success) {
+      RCLCPP_ERROR(node_->get_logger(), "goto failed with message: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  this->sleep(5.0);
+
+  if (uh->isFlyingNormally()) {
+    return true;
+  } else {
+    RCLCPP_ERROR(node_->get_logger(), "not flying normally");
+    return false;
+  }
+}
+
+int main(int argc, char* argv[]) {
+
+  rclcpp::init(argc, argv);
+
+  bool test_result = true;
+
+  Tester tester;
+
+  test_result &= tester.test();
+
+  tester.sleep(2.0);
+
+  std::cout << "Test: reporting test results" << std::endl;
+
+  tester.reportTestResult(test_result);
+
+  tester.join();
+}
